8f1dd7fc76058c5e78229df42f0b32fb2c2e5dd5
CoreyTang <1249728624@qq.com>
2020-01-11 21:18:32 +0800

    修改了User

--- src/main/java/com/corey/pojo/User.java
+++ src/main/java/com/corey/pojo/User.java
@@ -8,13 +8,23 @@ package com.corey.pojo;
 public class User {
     private String name;
     private String email;
+    private Integer age;
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
 
     public User() {
     }
 
-    public User(String name, String email) {
+    public User(String name, String email, Integer age) {
         this.name = name;
         this.email = email;
+        this.age = age;
     }
 
     public String getName() {
